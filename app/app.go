95b4be2c4d6261e9bacfe9bbb5c31edbcbf20e83
osawa-koki <github@osawa.dev>
2022-12-28 20:23:01 +0900

    小さな修正。

--- app/app.go
+++ app/app.go
@@ -13,20 +13,21 @@ func Start() {
 	// ハンドラファンクション -> デフォルトmultiplexerに登録。 | 自身で作成したmultiplexerを使用
 	// 第一引数 -> パターン
 	// 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
-	router.HandleFunc("/api/greet", greet)
-	router.HandleFunc("/api/customers", getAllCustomers)
-	router.HandleFunc("/api/customers_xml", getAllCustomers_xml)
-	router.HandleFunc("/api/customers_flex", getAllCustomers_flex)
-	router.HandleFunc("/api/customers/{customer_id}", getCustomer)
-	router.HandleFunc("/api/customers_int/{customer_id:[0-9]+}", getCustomer_int)
+	router.HandleFunc("/", greet)
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/customers_xml", getAllCustomers_xml)
+	router.HandleFunc("/customers_flex", getAllCustomers_flex)
+	router.HandleFunc("/customers/{customer_id}", getCustomer)
+	router.HandleFunc("/customers_int/{customer_id:[0-9]+}", getCustomer_int)
 
 	// HTTP動詞によるルーティング制御
-	router.HandleFunc("/api/methods", ofGet).Methods(http.MethodGet)
-	router.HandleFunc("/api/methods", ofPost).Methods(http.MethodPost)
-	router.HandleFunc("/api/methods", ofPut).Methods(http.MethodPut)
-	router.HandleFunc("/api/methods", ofDelete).Methods(http.MethodDelete)
+	router.HandleFunc("/methods", ofGet).Methods(http.MethodGet)
+	router.HandleFunc("/methods", ofPost).Methods(http.MethodPost)
+	router.HandleFunc("/methods", ofPut).Methods(http.MethodPut)
+	router.HandleFunc("/methods", ofDelete).Methods(http.MethodDelete)
 
 	// 第一引数 -> リッスンするアドレス
 	// 第二引数 -> 使用するmultiplexer | デフォルトで用意されているものを使用するため、nilを設定
-	http.ListenAndServe("0.0.0.0:8080", router)
+	http.ListenAndServe("0.0.0.0:80", router)
 }
