fa0d331e12853687da345f5451548c4d2b75f194
osawa-koki <github@osawa.dev>
2022-12-28 18:22:47 +0900

    fst commit;;

---
+++ app/app.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Start() {
+	// 自身でmultiplexerを登録
+	router := mux.NewRouter()
+
+	// ハンドラファンクション -> デフォルトmultiplexerに登録。 | 自身で作成したmultiplexerを使用
+	// 第一引数 -> パターン
+	// 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/customers_xml", getAllCustomers_xml)
+	router.HandleFunc("/customers_flex", getAllCustomers_flex)
+	router.HandleFunc("/customers/{customer_id}", getCustomer)
+	router.HandleFunc("/customers_int/{customer_id:[0-9]+}", getCustomer_int)
+
+	// HTTP動詞によるルーティング制御
+	router.HandleFunc("/methods", ofGet).Methods(http.MethodGet)
+	router.HandleFunc("/methods", ofPost).Methods(http.MethodPost)
+	router.HandleFunc("/methods", ofPut).Methods(http.MethodPut)
+	router.HandleFunc("/methods", ofDelete).Methods(http.MethodDelete)
+
+	// 第一引数 -> リッスンするアドレス
+	// 第二引数 -> 使用するmultiplexer | デフォルトで用意されているものを使用するため、nilを設定
+	http.ListenAndServe("localhost:8000", router)
+}
---
+++ app/handlers.go
@@ -0,0 +1,98 @@
+package app
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// JSON解析用に使用する構造体の定義
+// JSON出力用のキーは「`json:"★★★"`」と指定することが可能。
+// 指定しなければ、構造体のキーがそのまま使用される。
+// XMLを使用する場合には「xml:"★★★"」を書く。
+// ひとつの構造体をJSONとXMLで共有することも可能。
+type Customer struct {
+	Name    string `json:"fill_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+		{Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+		{Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+	}
+
+	// レスポンスヘッダにコンテントタイプを指定
+	w.Header().Add("Content-Type", "application/json")
+
+	// JSON形式にエンコード
+	// IOライターを受け取る。(FPrint関数的な、、、)
+	json.NewEncoder(w).Encode(customers)
+}
+
+func getAllCustomers_xml(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+		{Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+		{Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+	}
+
+	// レスポンスヘッダにコンテントタイプを指定
+	w.Header().Add("Content-Type", "application/xml")
+
+	// XML形式にエンコード
+	xml.NewEncoder(w).Encode(customers)
+}
+
+func getAllCustomers_flex(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Osawa Koki", City: "Soka", Zipcode: "340-0021"},
+		{Name: "Sakura Mana", City: "Shibuya", Zipcode: "150-0043"},
+		{Name: "Matz", City: "Tsukuba", Zipcode: "305-8577"},
+	}
+
+	if r.Header.Get("Accept") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	// パスパラメタを連想配列形式で取得
+	vars := mux.Vars(r)
+	id := vars["customer_id"]
+	fmt.Fprint(w, id)
+}
+
+func getCustomer_int(w http.ResponseWriter, r *http.Request) {
+	// パスパラメタを連想配列形式で取得
+	vars := mux.Vars(r)
+	id := vars["customer_id"]
+	fmt.Fprint(w, id)
+}
+
+// HTTP動詞によるルーティング制御用ハンドラ関数
+func ofGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I am a GET man")
+}
+func ofPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I am a POST man")
+}
+func ofPut(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I am a PUT man")
+}
+func ofDelete(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I am a DELETE man")
+}
